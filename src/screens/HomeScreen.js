4540c817e9a6bba21dfff65366e54fabea07b220
viniciusjps <vinicius.jorge.silva@ccc.ufcg.edu.br>
2021-10-24 11:32:55 -0300

    remover imports

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/core';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 import Header from '../components/Header';
 import ChampionList from '../components/ChampionList';
